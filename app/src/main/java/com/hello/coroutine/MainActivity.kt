2c684b44f14a0afa6def8f82aced7e85f9d2885b
peterlee <foxworld@gmail.com>
2024-06-26 09:55:54 +0900

    Initial commit

---
+++ app/src/main/java/com/hello/coroutine/MainActivity.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright© 2024. PETER HOON LEE, All rights reserved.
+ * @Name : PETER HOON LEE
+ * @Email : foxworld@gmail.com
+ * @Created Date : 2024-6-26
+ */
+
+package com.hello.coroutine
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
